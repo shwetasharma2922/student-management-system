a9b63a0f48bb48038a4766c68130a38736416f65
shwetasharma2922 <shwetasharma2922@gmail.com>
2020-05-13 14:05:25 +0530

    Initial commit

---
+++ admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title> Student Management System </title>
+</head>
+
+<body>
+	<h1 align="center"> STUDENT MANAGEMENT SYSTEM </h1>
+	<br><form align="center">
+	<div>
+		<label> Username: </label>
+		<input type="text" placeholder="Enter Username" name="username" required><br><br><br>
+		<label>Password: </label>
+		<input type="password" placeholder="Enter Password" name="password" required><br><br><br>
+		<button type="submit">Login</button>
+	</div>
+	</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title> Student Management System </title>
+</head>
+
+<body style="background: Red">
+	<h1 align="center" style="color: White"> STUDENT MANAGEMENT SYSTEM </h1>
+	<!--
+	<a href="admin" align="right"> Admin Login </a> <br>
+	-->
+	<br><form align="center" method="POST">
+	<div>
+		<label style="size: 100px;"> Username: </label>
+		<input type="text" align="center" placeholder="Enter Username" name="username" required><br><br><br>
+		<label>Password: </label>
+		<input type="password" align="center" placeholder="Enter Password" name="password" required><br><br><br>
+		<button type="submit" >Login</button>
+	</div>
+	</form>
+</body>
+</html>
